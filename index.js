55a6a54596aa12cc76f95c3d9ecbbff3e6a7ff39
Matthew Caruana Galizia <mattcg@gmail.com>
2017-06-21 12:09:29 +0200

    Better handling options.host as opposed to options.hostname and options.port

--- index.js
+++ index.js
@@ -6,6 +6,19 @@ var Socket = require('./lib/Socket');
 exports.Socket = Socket;
 
 exports.createConnection = function(options) {
-	var host = options.hostname || (options.host && options.host.split(":")[0])
-	return new Socket(options).connect(options.port, host);
+	var host, hostname = options.hostname, port = options.port;
+
+	if (options.host && (!hostname || !port)) {
+		host = options.host.split(':');
+	}
+
+	if (!port && host) {
+		port = parseInt(host[1], 10) || 0;
+	}
+
+	if (!hostname && host) {
+		hostname = host[0];
+	}
+
+	return new Socket(options).connect(port, hostname);
 };
